c92b1dae7316c94e1a9e3ee3a4b743c5414b54ea
Arne-Sebastian Winter <aswinter90@googlemail.com>
2024-12-20 14:02:15 +0100

    Extend demo view

--- SwiftUIDrawer-Demo/SwiftUIDrawer-Demo/ContentView.swift
+++ SwiftUIDrawer-Demo/SwiftUIDrawer-Demo/ContentView.swift
@@ -5,41 +5,111 @@ struct ContentView: View {
     @State private var drawerState = DrawerState(case: .partiallyOpened)
     @State private var drawerMinHeight = DrawerMinHeight.sameAsStickyHeaderContentHeightRelativeToTabBar(0)
     
+    @State private var isTabBarShown = true
+    @State private var isStickyHeaderShown = true
+    
     var body: some View {
-        TabView {
-            VStack {
-                Image(systemName: "globe")
-                    .imageScale(.large)
-                    .foregroundStyle(.tint)
-                
-                Text("Hello, world!")
+        ZStack {
+            tabViewIfNeeded {
+                backgroundView
+                    .drawerOverlay(
+                        state: $drawerState,
+                        minHeight: $drawerMinHeight,
+                        isTabBarShown: isTabBarShown,
+                        stickyHeader: isStickyHeaderShown ? { stickyDrawerHeader } : nil,
+                        content: { drawerContent }
+                    )
             }
-            .offset(.init(width: 0, height: -100))
+        }
+        .onChange(of: isStickyHeaderShown) { newValue in
+            updateDrawerState(
+                isStickyHeaderShown: newValue,
+                isTabBarShown: isTabBarShown
+            )
+        }
+        .onChange(of: isTabBarShown) { newValue in
+            updateDrawerState(
+                isStickyHeaderShown: isStickyHeaderShown,
+                isTabBarShown: newValue
+            )
+        }
+    }
+    
+    @ViewBuilder
+    func tabViewIfNeeded<Content: View>(content: () -> Content) -> some View {
+        if isTabBarShown {
+            TabView { content() }
+        } else {
+            ZStack { content() }
+        }
+    }
+    
+    var backgroundView: some View {
+        Color(.systemBackground)
             .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .overlay(alignment: .top) { configButtons }
             .tabItem { Label("Demo", systemImage: "wrench.and.screwdriver") }
             .opaqueTabBarStyle()
-            .drawerOverlay(
-                state: $drawerState,
-                minHeight: $drawerMinHeight,
-                isTabBarShown: true,
-                stickyHeader: {
-                    Text("Sticky header")
-                        .font(.title)
-                        .frame(maxWidth: .infinity)
-                        .padding()
-                        .border(Color.black.opacity(0.8))
-                        .padding()
-                },
-                content: {
-                    VStack(spacing: 16) {
-                        ForEach(0..<100) { index in
-                            Text("Content \(index)")
-                                .frame(maxWidth: .infinity)
-                                .padding(.horizontal, 16)
-                        }
-                    }
-                }
-            )
+    }
+    
+    var configButtons: some View {
+        VStack {
+            Text("Align to:")
+            
+            HStack(spacing: 16) {
+                Button("Safe area") { isTabBarShown = false }
+                    .disabled(!isTabBarShown)
+                
+                Button("Tab bar") { isTabBarShown = true }
+                    .disabled(isTabBarShown)
+            }
+            
+            Text("Show sticky header")
+                .padding(.top)
+            Toggle("", isOn: $isStickyHeaderShown)
+                .frame(maxWidth: 51)
+        }
+        .padding()
+    }
+    
+    var stickyDrawerHeader: some View {
+        VStack {
+            Text("Sticky header")
+                .font(.title)
+                .frame(maxWidth: .infinity)
+                .padding()
+                .padding(.horizontal)
+            
+            Color.black.opacity(0.3)
+                .frame(height: 1)
+        }
+    }
+    
+    var drawerContent: some View {
+        VStack(spacing: 16) {
+            Text("Content")
+                .font(.title2)
+                .padding(.top)
+            
+            ForEach(0..<30) { index in
+                Text("Item \(index)")
+                    .frame(maxWidth: .infinity)
+                    .padding(.horizontal, 16)
+            }
+            .padding(.vertical)
+        }
+    }
+    
+    func updateDrawerState(isStickyHeaderShown: Bool, isTabBarShown: Bool) {
+        switch (isStickyHeaderShown, isTabBarShown) {
+        case (true, true):
+            drawerMinHeight = .sameAsStickyHeaderContentHeightRelativeToTabBar(0)
+        case (true, false):
+            drawerMinHeight = .sameAsStickyHeaderContentHeightRelativeToSafeAreaBottom(0)
+        case (false, true):
+            drawerMinHeight = .relativeToSafeAreaBottomAndTabBar(0)
+        case (false, false):
+            drawerMinHeight = .relativeToSafeAreaBottom(0)
         }
     }
 }
@@ -49,7 +119,7 @@ extension View {
         if #available(iOS 16.0, *) {
             self
                 .toolbarBackground(.visible, for: .tabBar)
-                .toolbarBackground(Color.white, for: .tabBar)
+                .toolbarBackground(Color(.secondarySystemFill), for: .tabBar)
         } else {
             self
         }
