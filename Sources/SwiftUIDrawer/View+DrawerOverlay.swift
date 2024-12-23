94ac81f7119da625f5f6938d802bfc924fd16ca6
Arne-Sebastian Winter <aswinter90@googlemail.com>
2024-12-23 13:16:37 +0100

    Add a minimal demo view

--- Sources/SwiftUIDrawer/View+DrawerOverlay.swift
+++ Sources/SwiftUIDrawer/View+DrawerOverlay.swift
@@ -2,32 +2,33 @@ import SwiftUI
 
 public extension View {
     @ViewBuilder
-    func drawerOverlay<StickyHeaderContent: View>(
+    func drawerOverlay(
         state: Binding<DrawerState>,
         minHeight: Binding<DrawerMinHeight> = .constant(.relativeToSafeAreaBottom(0)),
         mediumHeight: Binding<DrawerMediumHeight>? = .constant(DrawerConstants.drawerDefaultMediumHeight),
         maxHeight: Binding<DrawerMaxHeight> = .constant(.relativeToSafeAreaTop(0)),
         isDimmingBackground: Bool = false,
-        @ViewBuilder stickyHeader: () -> StickyHeaderContent? = { nil },
+        @ViewBuilder stickyHeader: () -> some View = { EmptyView() },
         @ViewBuilder content: () -> some View
     ) -> some View {
-        dimmedDrawerBackground(
-            isShown: isDimmingBackground,
-            drawerState: state,
-            drawerMinHeight: minHeight,
-            drawerMediumHeight: mediumHeight,
-            drawerMaxHeight: maxHeight
-        )
-        .overlay(alignment: .bottom, content: {
-            Drawer(
-                state: state,
-                minHeight: minHeight,
-                mediumHeight: mediumHeight,
-                maxHeight: maxHeight,
-                stickyHeader: stickyHeader(),
-                content: content()
+        frame(maxWidth: .infinity, maxHeight: .infinity)
+            .dimmedDrawerBackground(
+                isShown: isDimmingBackground,
+                drawerState: state,
+                drawerMinHeight: minHeight,
+                drawerMediumHeight: mediumHeight,
+                drawerMaxHeight: maxHeight
             )
-        })
-        .ignoresSafeArea(.container, edges: .bottom)
+            .overlay(alignment: .bottom, content: {
+                Drawer(
+                    state: state,
+                    minHeight: minHeight,
+                    mediumHeight: mediumHeight,
+                    maxHeight: maxHeight,
+                    stickyHeader: stickyHeader(),
+                    content: content()
+                )
+            })
+            .ignoresSafeArea(.container, edges: .bottom)
     }
 }
---
+++ SwiftUIDrawer-Demo/SwiftUIDrawer-Demo/MinimalContentView.swift
@@ -0,0 +1,37 @@
+import SwiftUI
+import SwiftUIDrawer
+
+struct MinimalContentView: View {
+    @State private var drawerState = DrawerState.init(case: .partiallyOpened)
+    
+    var body: some View {
+        AppleLogo()
+            .padding(.top, 8)
+            .drawerOverlay(
+                state: $drawerState,
+                content: {
+                    VStack(spacing: 16) {
+                        ForEach(0..<30) { index in
+                            Text("Item \(index)")
+                                .frame(maxWidth: .infinity)
+                        }
+                    }
+                    .padding(.top, 8)
+                }
+            )
+    }
+}
+
+#Preview {
+    MinimalContentView()
+}
+
+private struct AppleLogo: View {
+    var body: some View {
+        Image(systemName: "apple.logo")
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .frame(width: 64, height: 64)
+            .frame(maxHeight: .infinity, alignment: .top)
+    }
+}
