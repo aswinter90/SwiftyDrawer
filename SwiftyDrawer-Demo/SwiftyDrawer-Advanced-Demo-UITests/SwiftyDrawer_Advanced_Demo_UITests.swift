90482e3b37e68a8360102532432a22f5f9a2b650
Arne-Sebastian Winter <aswinter90@googlemail.com>
2025-02-26 11:59:04 +0100

    Clean up UI test comments

--- SwiftyDrawer-Demo/SwiftyDrawer-Advanced-Demo-UITests/SwiftyDrawer_Advanced_Demo_UITests.swift
+++ SwiftyDrawer-Demo/SwiftyDrawer-Advanced-Demo-UITests/SwiftyDrawer_Advanced_Demo_UITests.swift
@@ -8,16 +8,7 @@ final class SwiftyDrawer_DemoUITests: XCTestCase {
     private static let lastDrawerContentItemIdentifier = "Item 29"
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
     @MainActor
@@ -28,7 +19,7 @@ final class SwiftyDrawer_DemoUITests: XCTestCase {
         let drawer = app.otherElements.matching(identifier: "SwiftyDrawer").firstMatch
         XCTAssertTrue(drawer.exists)
 
-        // Unfortunately there is no UIWindow instance available in XCTestCases to get the safe-area-insets from, hence the insets are injected as an accessibility label in the UI hierarchy of the demo app. Which is a hack, but... 🙄
+        // Unfortunately there is no UIWindow instance available in XCTestCases to get the safe-area-insets from, hence the insets are injected as an accessibility label in the UI hierarchy of the drawer for now, which is not an elegant solution.
         let safeAreaInsets = try SafeAreaInsetsHolder(data: drawer.label.data(using: .utf8)!)
 
         // Check drawer top position
