b1f9be3cb0db48261d4f5ea6b81936900eaee197
Arne-Sebastian Winter <aswinter90@googlemail.com>
2025-02-28 12:59:24 +0100

    Refactor the basic demo for a more appealing appearance

--- SwiftyDrawer-Demo/SwiftyDrawer-Basic-Demo/ContentView.swift
+++ SwiftyDrawer-Demo/SwiftyDrawer-Basic-Demo/ContentView.swift
@@ -5,14 +5,17 @@ struct ContentView: View {
     @State private var drawerState = DrawerState.init(case: .partiallyOpened)
 
     var body: some View {
-        AppleLogo()
-            .offset(y: -(UIScreen.main.bounds.height / 4))
+        MyAppleLogo()
             .drawerOverlay(
                 state: $drawerState,
                 content: {
-                    VStack(spacing: 16) {
+                    VStack {
                         ForEach(0..<30) { index in
                             Text("Item \(index)")
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                                .padding()
+
+                            Divider()
                         }
                     }
                     .padding(.top, 8)
@@ -25,11 +28,16 @@ struct ContentView: View {
     ContentView()
 }
 
-private struct AppleLogo: View {
+private struct MyAppleLogo: View {
     var body: some View {
-        Image(systemName: "apple.logo")
-            .resizable()
-            .aspectRatio(contentMode: .fit)
-            .frame(width: 128, height: 128)
+        GeometryReader { proxy in
+            Image(systemName: "apple.logo")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 128, height: 128)
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .offset(y: -(proxy.size.height / 4))
+                .background(Color.gray.opacity(0.5))
+        }
     }
 }
